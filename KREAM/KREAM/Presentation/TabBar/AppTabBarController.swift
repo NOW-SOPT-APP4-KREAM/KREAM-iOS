7f1f64221ba2d56ad6f93dff1cf94b3b7fd3c9dd
FpRaArNkK <mspark1184@naver.com>
2024-05-13 13:46:36 +0900

    Feat: AppTabBarController 작성 완료

--- KREAM/KREAM/Presentation/TabBar/AppTabBarController.swift
+++ KREAM/KREAM/Presentation/TabBar/AppTabBarController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+/// App의 하단 TabBarController 입니다.
 final class AppTabBarController: UITabBarController {
     
     // MARK: Life Cycle - viewDidLoad
@@ -18,29 +19,33 @@ final class AppTabBarController: UITabBarController {
     
     // MARK: setUpView
     private func setUpView() {
+    
         let homeViewController = UIViewController()
-        homeViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
-        
         let styleViewController = UIViewController()
-        styleViewController.tabBarItem = UITabBarItem(title: "HOME", image: UIImage(named: "first"), tag: 1)
-        styleViewController.tabBarItem.setTitleTextAttributes(AppleSDGothicNeo.Body7(.bold).attributes, for: .normal)
-        styleViewController.tabBarItem.image = UIImage(named: "first")?.withRenderingMode(.alwaysOriginal) // 이미지 설정 및 템플릿 모드 비활성화
-                
         let shopViewController = UIViewController()
-        shopViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 2)
-        
         let savedViewController = UIViewController()
-        savedViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 3)
-        
         let myViewController = UIViewController()
-        myViewController.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 4)
         
-        setViewControllers([homeViewController, styleViewController, shopViewController, savedViewController, myViewController], animated: true)
+        let viewControllerList = [
+            homeViewController,
+            styleViewController,
+            shopViewController,
+            savedViewController,
+            myViewController
+        ]
+        
+        viewControllerList.enumerated().forEach { index, viewController in
+            let tab = AppTab.allCases[index]
+            viewController.tabBarItem = UITabBarItem(title: tab.rawValue, image: tab.image, tag: index)
+            viewController.tabBarItem.setTitleTextAttributes(AppleSDGothicNeo.Body7(.bold).attributes, for: .normal)
+        }
+        
+        self.setViewControllers(viewControllerList, animated: true)
     }
     
     // MARK: setUpStyle
     private func setUpStyle() {
-        self.tabBar.tintColor = .white
+        self.tabBar.tintColor = .black02
         self.tabBar.barTintColor = .white
         self.tabBar.backgroundColor = .white
         
