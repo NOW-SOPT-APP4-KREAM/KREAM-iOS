b4bca0595d56c47e0a8d27115d0ef4fa8f3cf2b8
FpRaArNkK <mspark1184@naver.com>
2024-05-21 01:33:15 +0900

    Feat: #7 - ItemDetail Model 추가

---
+++ KREAM/KREAM/Presentation/Components/ItemInfoDetailCollectionViewCell /ItemDetail.swift
@@ -0,0 +1,28 @@
+//
+//  ItemDetail.swift
+//  KREAM
+//
+//  Created by 박민서 on 5/21/24.
+//
+
+/// Item의 디테일 정보를 표시하기 위한 모델입니다
+/// - 사용 Views : asdfView
+struct ItemDetail {
+    // item internal info
+    let itemId: Int
+    // top info
+    var isPreviouslySeen: Bool // 앱 내에서 변경될 여지가 있는 변수는 var
+    let tradeVolume: Int? // 처음 생성 시를 제외하고 변경되지 않으면 let
+    let imageUrl: String
+    let isBookmarked: Bool? // 전달되지 않을 수도 있으면 optional
+    // middle info
+    let brandName: String?
+    let englishName: String
+    let koreanName: String?
+    // bottom info
+    let isExpress: Bool
+    let price: Int
+    let isBuyNowPrice: Bool
+    let bookmarkCount: Int?
+    let heartCount: Int?
+}
