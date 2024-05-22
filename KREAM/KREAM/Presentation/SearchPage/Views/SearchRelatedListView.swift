53bbbdd8137bb89a5fb0ff78e6d3d3c1f93f26d0
FpRaArNkK <mspark1184@naver.com>
2024-05-23 03:25:23 +0900

    Feat: #25 - SearchRelatedListView 상단 섹션 작성 완료

--- KREAM/KREAM/Presentation/SearchPage/Views/SearchRelatedListView.swift
+++ KREAM/KREAM/Presentation/SearchPage/Views/SearchRelatedListView.swift
@@ -13,6 +13,9 @@ final class SearchRelatedListView: UIView {
     
     // MARK: Properties
     // MARK: Views
+    // top section
+    private let relatedLabel = UILabel()
+    
     // MARK: Init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,16 +33,37 @@ final class SearchRelatedListView: UIView {
     private func setUpViews() {
         
     }
+    
     // MARK: setUpLayout
     private func setUpLayout() {
-        
+        [
+            relatedLabel
+        ].forEach { self.addSubview($0) }
     }
+    
     // MARK: setUpStyle
     private func setUpStyle() {
         
+        relatedLabel.do {
+            $0.attributedText = "연관 검색어".toKreamFontString(.body2(.bold), textColor: .black02)
+        }
     }
     // MARK: setUpConstraint
     private func setUpConstraint() {
         
+        relatedLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(22)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
     }
+}
+
+#Preview {
+    PreviewController(SearchRelatedListView(), snp: { view in
+        view.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(375)
+        }
+    })
 }
