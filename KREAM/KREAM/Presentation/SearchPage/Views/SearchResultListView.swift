99438af1a7180e7bdd42b1bae924c78b91165eab
FpRaArNkK <mspark1184@naver.com>
2024-05-23 01:40:04 +0900

    Feat: #21 - SearchResultListView init

---
+++ KREAM/KREAM/Presentation/SearchPage/Views/SearchResultListView.swift
@@ -0,0 +1,43 @@
+//
+//  SearchResultListView.swift
+//  KREAM
+//
+//  Created by 박민서 on 5/23/24.
+//
+
+import UIKit
+
+final class SearchResultListView: UIView {
+    
+    // MARK: Properties
+    // MARK: Views
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+        setUpLayout()
+        setUpStyle()
+        setUpConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: setUpViews
+    private func setUpViews() {
+        
+    }
+    // MARK: setUpLayout
+    private func setUpLayout() {
+        
+    }
+    // MARK: setUpStyle
+    private func setUpStyle() {
+        
+    }
+    // MARK: setUpConstraint
+    private func setUpConstraint() {
+        
+    }
+}
