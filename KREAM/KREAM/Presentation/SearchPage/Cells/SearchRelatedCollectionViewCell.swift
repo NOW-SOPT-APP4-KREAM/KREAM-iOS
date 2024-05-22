3ea8bc87c48ed1fedbc0a291c69dd4448e21c6ad
FpRaArNkK <mspark1184@naver.com>
2024-05-23 03:26:36 +0900

    Feat: #25 - SearchRelatedCollectionViewCell init

---
+++ KREAM/KREAM/Presentation/SearchPage/Cells/SearchRelatedCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  SearchRelatedCollectionViewCell.swift
+//  KREAM
+//
+//  Created by 박민서 on 5/23/24.
+//
+
+import SnapKit
+import Then
+import UIKit
+
+final class SearchRelatedCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Properties
+    // MARK: Views
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+        setUpLayout()
+        setUpStyle()
+        setUpConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: setUpViews
+    private func setUpViews() {
+        
+    }
+    // MARK: setUpLayout
+    private func setUpLayout() {
+        
+    }
+    // MARK: setUpStyle
+    private func setUpStyle() {
+        
+    }
+    // MARK: setUpConstraint
+    private func setUpConstraint() {
+        
+    }
+}
