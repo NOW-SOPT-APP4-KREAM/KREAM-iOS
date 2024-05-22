f0e70cfc8c13e89ba68071ed87ecb07018084b0f
FpRaArNkK <mspark1184@naver.com>
2024-05-22 21:09:34 +0900

    Feat: #13 - init SearchSegmentControlView

---
+++ KREAM/KREAM/Global/Utility/Divider.swift
@@ -0,0 +1,8 @@
+//
+//  Divider.swift
+//  KREAM
+//
+//  Created by 박민서 on 5/22/24.
+//
+
+import Foundation
---
+++ KREAM/KREAM/Presentation/SearchPage/Views/SearchSegmentControlView.swift
@@ -0,0 +1,15 @@
+//
+//  SearchSegmentControlView.swift
+//  KREAM
+//
+//  Created by 박민서 on 5/22/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class SearchSegmentControlView: UIView {
+    
+}
+
