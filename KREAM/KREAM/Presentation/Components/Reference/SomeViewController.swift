e5b06b33861b980383a057837d2d1789c3a85b18
FpRaArNkK <mspark1184@naver.com>
2024-05-22 20:28:21 +0900

    Feat: #13 - view, vc reference 추가

---
+++ KREAM/KREAM/Presentation/Components/Reference/SomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SomeViewController.swift
+//  KREAM
+//
+//  Created by 박민서 on 5/22/24.
+//
+
+import UIKit
+
+final class someViewController: UIViewController {
+    
+    // MARK: Properties
+    // MARK: Views
+    // MARK: Life Cycle - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpViews()
+        setUpLayout()
+        setUpStyle()
+        setUpConstraint()
+    }
+    // MARK: setUpViews
+    private func setUpViews() {
+        
+    }
+    // MARK: setUpLayout
+    private func setUpLayout() {
+        
+    }
+    // MARK: setUpStyle
+    private func setUpStyle() {
+        
+    }
+    // MARK: setUpConstraint
+    private func setUpConstraint() {
+        
+    }
+}
