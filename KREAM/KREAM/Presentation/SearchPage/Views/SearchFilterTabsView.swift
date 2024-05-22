a22f39e63a07fe7500ad0209749c9ad30dbc36cd
FpRaArNkK <mspark1184@naver.com>
2024-05-23 00:05:25 +0900

    Feat: #18 - Button UI Factory 로직 작성

--- KREAM/KREAM/Presentation/SearchPage/Views/SearchFilterTabsView.swift
+++ KREAM/KREAM/Presentation/SearchPage/Views/SearchFilterTabsView.swift
@@ -16,7 +16,6 @@ final class SearchFilterTabsView: UIView {
     // MARK: Views
     
     // MARK: Internal Logic
-
     
     // MARK: Init
     override init(frame: CGRect) {
@@ -52,4 +51,58 @@ final class SearchFilterTabsView: UIView {
     private func setUpConstraint() {
 
     }
+}
+
+// MARK: Internal Function
+private extension SearchFilterTabsView {
+    func applyButtonUI(to button: UIButton, leftImage: Bool = false, rightImage: Bool = false) {
+        var configuration = UIButton.Configuration.filled()
+        
+        // 배경 UI 설정
+        configuration.baseBackgroundColor = .clear
+        configuration.background.strokeColor = .gray04
+        configuration.background.strokeWidth = 1
+        configuration.background.cornerRadius = 20
+        configuration.contentInsets = .init(top: 7, leading: 11, bottom: 7, trailing: 11)
+        
+        // 글자 관련 설정
+        configuration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer { incoming in
+            var outgoing = incoming
+            outgoing.font = UIFont.kreamFont(.body4(.semibold, isLh150: false))
+            outgoing.foregroundColor = UIColor.black02
+            return outgoing
+        }
+        configuration.titleLineBreakMode = .byTruncatingTail
+        
+        // 좌측 이미지
+        if leftImage {
+            let image = UIImage(systemName: "bolt")?.resized(to: .init(width: 14, height: 16))?.withRenderingMode(.alwaysTemplate)
+            configuration.baseForegroundColor = .green02
+            configuration.image = image
+            configuration.imagePadding = 4
+            configuration.imagePlacement = .leading
+        }
+        
+        // 우측 이미지
+        if rightImage {
+            let image = UIImage(systemName: "chevron.down")?.resized(to: .init(width: 8, height: 4))?.withRenderingMode(.alwaysTemplate)
+            configuration.baseForegroundColor = .gray03
+            configuration.image = image
+            configuration.imagePadding = 4
+            configuration.imagePlacement = .trailing
+        }
+        
+        button.configuration = configuration
+    }
+}
+
+#Preview {
+//    PreviewController(SearchFilterTabsView(), snp: { view in
+//        view.snp.makeConstraints {
+//            $0.width.equalTo(120)
+//            $0.height.equalTo(60)
+//            $0.center.equalToSuperview()
+//        }
+//    })
+    SearchFilterTabsView()
 }
