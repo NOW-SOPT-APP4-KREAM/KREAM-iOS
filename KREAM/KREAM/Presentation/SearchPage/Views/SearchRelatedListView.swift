92108467431adab344645e670ddf0b8f50271468
FpRaArNkK <mspark1184@naver.com>
2024-05-23 03:20:41 +0900

    Feat: #25 - SearchRelatedListView init

---
+++ KREAM/KREAM/Presentation/SearchPage/Views/SearchRelatedListView.swift
@@ -0,0 +1,45 @@
+//
+//  SearchRelatedListView.swift
+//  KREAM
+//
+//  Created by 박민서 on 5/23/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class SearchRelatedListView: UIView {
+    
+    // MARK: Properties
+    // MARK: Views
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+        setUpLayout()
+        setUpStyle()
+        setUpConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: setUpViews
+    private func setUpViews() {
+        
+    }
+    // MARK: setUpLayout
+    private func setUpLayout() {
+        
+    }
+    // MARK: setUpStyle
+    private func setUpStyle() {
+        
+    }
+    // MARK: setUpConstraint
+    private func setUpConstraint() {
+        
+    }
+}
