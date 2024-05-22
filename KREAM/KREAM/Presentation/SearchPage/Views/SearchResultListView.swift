acd3ef7396e10ecfa87a4539f0f80d6e270ad425
FpRaArNkK <mspark1184@naver.com>
2024-05-23 02:25:22 +0900

    Feat: #21 - SearchResultListView 상단 뷰 작성 완료

--- KREAM/KREAM/Presentation/SearchPage/Views/SearchResultListView.swift
+++ KREAM/KREAM/Presentation/SearchPage/Views/SearchResultListView.swift
@@ -6,11 +6,22 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class SearchResultListView: UIView {
     
     // MARK: Properties
+    
     // MARK: Views
+    // top section
+    private let topSectionView = UIView()
+    private let leftStack = UIStackView()
+    private let magnifyImageView = UIImageView()
+    private let searchTextLabel = UILabel()
+    private let resultTextLabel = UILabel()
+    private let moreButton = UIButton()
+    
     // MARK: Init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -28,16 +39,89 @@ final class SearchResultListView: UIView {
     private func setUpViews() {
         
     }
+    
     // MARK: setUpLayout
     private func setUpLayout() {
         
+        // top section
+        [
+            magnifyImageView,
+            searchTextLabel,
+            resultTextLabel
+        ].forEach { leftStack.addArrangedSubview($0) }
+        
+        [
+            leftStack,
+            moreButton
+        ].forEach { topSectionView.addSubview($0) }
+        
+        [
+            topSectionView
+        ].forEach { self.addSubview($0) }
+        
     }
+    
     // MARK: setUpStyle
     private func setUpStyle() {
         
+        leftStack.do {
+            $0.axis = .horizontal
+            $0.spacing = 4
+        }
+        
+        magnifyImageView.do {
+            $0.image = UIImage(systemName: "magnifyingglass")
+            $0.tintColor = .black05
+        }
+        
+        searchTextLabel.do {
+            $0.text = "아디다스 흰색"
+            $0.font = .kreamFont(.body2(.bold))
+            $0.textColor = .black02
+        }
+        
+        resultTextLabel.do {
+            $0.attributedText = "검색결과".toKreamFontString(.body2(.bold), textColor: .black06)
+        }
+        
+        moreButton.do {
+            $0.setAttributedTitle(
+                "더보기".toKreamFontString(
+                    .body4(.bold, isLh150: false),
+                    textColor: .black03
+                ), for: .normal
+            )
+        }
     }
     // MARK: setUpConstraint
     private func setUpConstraint() {
         
+        // top section
+        topSectionView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(12)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(44)
+        }
+        
+        leftStack.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(10)
+        }
+        
+        moreButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-16)
+        }
+        
     }
+}
+
+#Preview {
+    PreviewController(SearchResultListView(), snp: { view in
+        view.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(215)
+        }
+    })
 }
