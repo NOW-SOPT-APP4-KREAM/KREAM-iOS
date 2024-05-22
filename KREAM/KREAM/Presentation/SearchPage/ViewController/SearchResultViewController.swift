03075a0fcea9f78a3ead6a0874ceeefc7a9dc06c
FpRaArNkK <mspark1184@naver.com>
2024-05-23 05:49:07 +0900

    Feat: #31 - SearchResultCollectionView 작성

--- KREAM/KREAM/Presentation/SearchPage/ViewController/SearchResultViewController.swift
+++ KREAM/KREAM/Presentation/SearchPage/ViewController/SearchResultViewController.swift
@@ -12,11 +12,24 @@ import Then
 final class SearchResultViewController: UIViewController {
     
     // MARK: Properties
+    private let cellType: ItemType = .full
+    private lazy var twoColumnLayout = UICollectionViewFlowLayout().then {
+        $0.itemSize = cellType.cellSize
+        $0.minimumLineSpacing = 7
+        $0.sectionInset = .init(top: 10, left: 16, bottom: 10, right: 16)
+    }
+    private lazy var threeColumnLayout = UICollectionViewFlowLayout().then {
+        $0.itemSize = CGSize(width: 109, height: cellType.cellSize.height)
+        $0.minimumLineSpacing = 7
+        $0.sectionInset = .init(top: 10, left: 16, bottom: 10, right: 16)
+    }
+    
     // MARK: Views
     private let topSearchBar = TopSearchBarView()
     private let searchSegmentControl = SearchSegmentControlView()
     private let filterTabView = SearchFilterTabsView()
     private let searchResultHeader = SearchResultHeaderView()
+    private lazy var searchResultCollectionView = UICollectionView(frame: .zero, collectionViewLayout: self.twoColumnLayout)
     private let searchRelatedListView = SearchRelatedListView()
     private let searchResultListView = SearchResultListView()
     
@@ -31,7 +44,8 @@ final class SearchResultViewController: UIViewController {
     
     // MARK: setUpViews
     private func setUpViews() {
-        
+        searchResultCollectionView.register(ItemInfoDetailCollectionViewCell.self, forCellWithReuseIdentifier: ItemInfoDetailCollectionViewCell.id)
+        searchResultCollectionView.dataSource = self
     }
     
     // MARK: setUpLayout
@@ -41,6 +55,7 @@ final class SearchResultViewController: UIViewController {
             searchSegmentControl,
             filterTabView,
             searchResultHeader,
+            searchResultCollectionView,
             searchRelatedListView,
             searchResultListView
         ].forEach { self.view.addSubview($0) }
@@ -77,8 +92,14 @@ final class SearchResultViewController: UIViewController {
             $0.height.equalTo(32)
         }
         
+        searchResultCollectionView.snp.makeConstraints {
+            $0.top.equalTo(searchResultHeader.snp.bottom).offset(12)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(1700)
+        }
+        
         searchResultListView.snp.makeConstraints {
-            $0.top.equalTo(searchResultHeader.snp.bottom).offset(7)
+            $0.top.equalTo(searchResultCollectionView.snp.bottom).offset(7)
             $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(310)
         }
@@ -88,6 +109,45 @@ final class SearchResultViewController: UIViewController {
             $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(215)
         }
+    }
+}
+
+// MARK: UICollectionViewDataSource
+extension SearchResultViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ItemInfoDetailCollectionViewCell.id,
+            for: indexPath
+        ) as? ItemInfoDetailCollectionViewCell else { return UICollectionViewCell() }
+        _ = cell.interface(
+            input: .init(
+                itemType: self.cellType,
+                itemDetail: .init(
+                    itemId: 1,
+                    isPreviouslySeen: false, // 고정
+                    tradeVolume: "1.5만",
+                    imageUrl: "",
+                    isBookmarked: nil,
+                    brandName: "ASDF",
+                    isCheck: false, // 고정
+                    englishName: "asdfasdfasdfkaujh",
+                    koreanName: "ㅁㄴㅇㄹㅁㄴㅇㄹ",
+                    isExpress: true,
+                    isCoupon: false,
+                    isFreeShip: false,
+                    price: "123124원",
+                    isBuyNowPrice: true,
+                    bookmarkCount: "1.4만",
+                    heartCount: "1.2만"
+                ),
+                bookmarkButtonDidTap: { print($0 ?? -1) }
+            )
+        )
+        return cell
     }
 }
 
