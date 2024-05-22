858389490aa20ce6f22b141290913ae199881354
FpRaArNkK <mspark1184@naver.com>
2024-05-22 21:09:58 +0900

    Feat: #13 - Divider 추가

--- KREAM/KREAM/Global/Utility/Divider.swift
+++ KREAM/KREAM/Global/Utility/Divider.swift
@@ -5,4 +5,16 @@
 //  Created by 박민서 on 5/22/24.
 //
 
-import Foundation
+import UIKit
+
+final class Divider: UIView {
+
+    init(color: UIColor) {
+        super.init(frame: .zero)
+        self.backgroundColor = color
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
