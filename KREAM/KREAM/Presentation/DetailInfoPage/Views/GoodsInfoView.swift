a40c830256c51ba053e3afc62e1b04d8e278552c
0gonge <Challengef0802@gmail.com>
2024-05-24 05:55:11 +0900

    Feat:#36 - GoodsInfoView

--- KREAM/KREAM/Presentation/DetailInfoPage/Views/GoodsInfoView.swift
+++ KREAM/KREAM/Presentation/DetailInfoPage/Views/GoodsInfoView.swift
@@ -23,25 +23,15 @@ class GoodsInfoView : UIView{
     }
     
     //MARK: - Set Elements
-    
-//    let collectionView: UICollectionView = {
-//        let flowlayout = UICollectionViewLayout()
-//        let cv = UICollectionView(frame: <#T##CGRect#>)
-//        return cv
-//    }()
-    
+    private let dataStackView = UIStackView()
+    private let scrollView = UIScrollView()
+    private let divider = UIView()
     private let priceSubLabel = UILabel()
-    
     private let priceLabel = UILabel()
-    
     private let productNameEng = UILabel()
-    
     private let productNameKor = UILabel()
-    
     private let buttonBound = UIView()
-    
     private let buttonLabel = UILabel()
-    
     private let downArrow = UIImageView()
     
     //MARK: - SetUp
@@ -78,13 +68,44 @@ class GoodsInfoView : UIView{
         downArrow.do {
             $0.image = UIImage(named: "icn_downarrow")
         }
+        
+        scrollView.do {
+            $0.showsHorizontalScrollIndicator = false
+        }
+        
+        dataStackView.do {
+            $0.spacing = 20
+        }
+
     }
     
     //MARK: - Layout
     private func setLayout(){
-        [priceSubLabel, priceLabel, productNameEng, productNameKor,buttonBound,buttonLabel,downArrow].forEach{
+                
+        [priceSubLabel, priceLabel, productNameEng, productNameKor,buttonBound,buttonLabel,downArrow, scrollView].forEach{
             self.addSubview($0)
         }
+        
+        scrollView.addSubview(dataStackView)
+             
+        let apiData = ["최근 거래가", "발매가", "모델번호", "출시일"]
+        let priceData = ["132,000원", "139,000원", "B75806", "2018/01/07"]
+        let waveData = ["16,000(-10.8)", nil, nil, nil]
+
+        for (index, item) in apiData.enumerated() {
+            let cell = ItemDataStackViewCell()
+            cell.configure(title: item, content: priceData[index], wave: waveData[index])
+            
+            let divider = UIView()
+            divider.snp.makeConstraints {
+                $0.width.equalTo(1)
+                $0.height.equalTo(45)
+            }
+            divider.backgroundColor = .gray06
+            dataStackView.addArrangedSubview(cell)
+            dataStackView.addArrangedSubview(divider)
+        }
+        
     }
     
     private func setConstraints(){
@@ -131,8 +152,28 @@ class GoodsInfoView : UIView{
             $0.trailing.equalTo(buttonBound.snp.trailing).inset(9)
             $0.width.height.equalTo(32)
         }
+        
+        scrollView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(45)
+            $0.top.equalTo(buttonBound.snp.bottom).offset(15)
+        }
+        
+        dataStackView.snp.makeConstraints {
+            $0.verticalEdges.equalTo(scrollView.contentLayoutGuide)
+            $0.horizontalEdges.equalTo(scrollView.contentLayoutGuide).inset(16)
+            $0.centerY.equalTo(scrollView.contentLayoutGuide)
+        }
+//        divider.snp.makeConstraints {
+//            $0.width.equalTo(1)
+//            $0.height.equalTo(45)
+//            $0.center.equalToSuperview()
+//        }
+        
+        
     }
 }
+
 #Preview {
     PreviewController(GoodsInfoView(), snp: { view in
         view.snp.makeConstraints {
