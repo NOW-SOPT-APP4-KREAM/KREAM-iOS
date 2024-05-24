4cec42b9d2a8740b520e2d0fc0e9ede205ddf522
FpRaArNkK <mspark1184@naver.com>
2024-05-24 19:52:04 +0900

    Feat: #54 - JustDroppedView init, DTO 수정

---
+++ KREAM/KREAM/Presentation/RecommendationPage/Views/JustDroppedView.swift
@@ -0,0 +1,43 @@
+//
+//  JustDroppedView.swift
+//  KREAM
+//
+//  Created by 박민서 on 5/24/24.
+//
+
+import Foundation
+
+final class JustDroppedView: UIView {
+    
+    // MARK: Properties
+    // MARK: Views
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+        setUpLayout()
+        setUpStyle()
+        setUpConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: setUpViews
+    private func setUpViews() {
+        
+    }
+    // MARK: setUpLayout
+    private func setUpLayout() {
+        
+    }
+    // MARK: setUpStyle
+    private func setUpStyle() {
+        
+    }
+    // MARK: setUpConstraint
+    private func setUpConstraint() {
+        
+    }
+}
