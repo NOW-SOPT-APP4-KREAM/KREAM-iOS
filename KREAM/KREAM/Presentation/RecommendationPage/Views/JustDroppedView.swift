f378c55e1eb0992675c66ec7edfa4d459309355b
FpRaArNkK <mspark1184@naver.com>
2024-05-24 20:08:04 +0900

    Feat: #54 JustDroppedView 헤더 작성

--- KREAM/KREAM/Presentation/RecommendationPage/Views/JustDroppedView.swift
+++ KREAM/KREAM/Presentation/RecommendationPage/Views/JustDroppedView.swift
@@ -5,12 +5,26 @@
 //  Created by 박민서 on 5/24/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
 
 final class JustDroppedView: UIView {
     
     // MARK: Properties
+    private let itemList: [ItemDetail] = []
+    private let justDroppedLabel = UILabel()
+    private let justDroppedKoreanLabel = UILabel()
+    private let showMoreButton = UIButton()
+    
     // MARK: Views
+    private let justDroppedCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        $0.itemSize = ItemType.full.cellSize
+        $0.minimumLineSpacing = 7
+        $0.scrollDirection = .horizontal
+        $0.sectionInset = .init(top: 0, left: 16, bottom: 0, right: 16)
+    })
+    
     // MARK: Init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,18 +40,59 @@ final class JustDroppedView: UIView {
     
     // MARK: setUpViews
     private func setUpViews() {
-        
+        justDroppedCollectionView.register(ItemInfoDetailCollectionViewCell.self, forCellWithReuseIdentifier: ItemInfoDetailCollectionViewCell.id)
     }
+    
     // MARK: setUpLayout
     private func setUpLayout() {
-        
+        [
+            justDroppedLabel,
+            justDroppedKoreanLabel,
+            showMoreButton,
+            justDroppedCollectionView
+        ].forEach { self.addSubview($0) }
     }
+    
     // MARK: setUpStyle
     private func setUpStyle() {
+        justDroppedLabel.do {
+            $0.attributedText = "Just Dropped".toKreamFontString(.head2(.heavy), textColor: .black02)
+        }
         
+        justDroppedKoreanLabel.do {
+            $0.attributedText = "발매 상품".toKreamFontString(.custom(size: 13, weight: .semibold), textColor: .black06)
+        }
+        
+        showMoreButton.do {
+            $0.setAttributedTitle("더보기".toKreamFontString(.body4(.bold, isLh150: false), textColor: .black06), for: .normal)
+        }
     }
+    
     // MARK: setUpConstraint
     private func setUpConstraint() {
+        justDroppedLabel.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(22)
+            $0.leading.equalToSuperview().offset(16)
+        }
         
+        justDroppedKoreanLabel.snp.makeConstraints {
+            $0.top.equalTo(justDroppedLabel.snp.bottom).offset(2)
+            $0.leading.equalTo(justDroppedLabel)
+        }
+        
+        showMoreButton.snp.makeConstraints {
+            $0.centerY.equalTo(justDroppedKoreanLabel)
+            $0.trailing.equalToSuperview().offset(-16)
+        }
     }
+}
+
+#Preview {
+    PreviewController(JustDroppedView(), snp: {
+        $0.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(363)
+        }
+    })
 }
