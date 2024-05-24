5531f575b83f7df265dec911240dee11c6ef32b0
ParkSY0919 <qhr498@gmail.com>
2024-05-24 16:25:03 +0900

    Del: #46 - 더미 코드 삭제

--- KREAM/KREAM/Presentation/ReleaseInfoPage/Model/ItemModel.swift
+++ KREAM/KREAM/Presentation/ReleaseInfoPage/Model/ItemModel.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 struct ItemModel {
-   let itemImg: UIImage
+   var itemURL: String
    let brandTitle: String
    let engTitle: String
    var isScrap: Bool
@@ -16,103 +16,103 @@ struct ItemModel {
    var isNew: Bool
 }
 
-extension ItemModel {
-   static func dummy() -> [ItemModel] {
-      return [
-         ItemModel(
-            itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Cloud White",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         ),
-         ItemModel(
-            itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Black White Gum",
-            isScrap: true,
-            isUpdate: true,
-            isNew: false
-         ),
-         ItemModel(itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Cloud White",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         ),
-         ItemModel(itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Black White Gum",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         ),
-         ItemModel(
-            itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Cloud White",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         ),
-         ItemModel(
-            itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Black White Gum",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         ),
-         ItemModel(
-            itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Cloud White",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         ),
-         ItemModel(
-            itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Black White Gum",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         ),
-         ItemModel(
-            itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Cloud White",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         ),
-         ItemModel(
-            itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Black White Gum",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         ),
-         ItemModel(
-            itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Cloud White",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         ),
-         ItemModel(
-            itemImg: UIImage(resource: .icnShop),
-            brandTitle: "Adidas",
-            engTitle: "Adidas Samba OG Black White Gum",
-            isScrap: false,
-            isUpdate: false,
-            isNew: true
-         )
-      ]
-   }
-}
+//extension ItemModel {
+//   static func dummy() -> [ItemModel] {
+//      return [
+//         ItemModel(
+//            itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Cloud White",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         ),
+//         ItemModel(
+//            itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Black White Gum",
+//            isScrap: true,
+//            isUpdate: true,
+//            isNew: false
+//         ),
+//         ItemModel(itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Cloud White",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         ),
+//         ItemModel(itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Black White Gum",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         ),
+//         ItemModel(
+//            itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Cloud White",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         ),
+//         ItemModel(
+//            itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Black White Gum",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         ),
+//         ItemModel(
+//            itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Cloud White",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         ),
+//         ItemModel(
+//            itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Black White Gum",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         ),
+//         ItemModel(
+//            itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Cloud White",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         ),
+//         ItemModel(
+//            itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Black White Gum",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         ),
+//         ItemModel(
+//            itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Cloud White",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         ),
+//         ItemModel(
+//            itemImg: UIImage(resource: .icnShop),
+//            brandTitle: "Adidas",
+//            engTitle: "Adidas Samba OG Black White Gum",
+//            isScrap: false,
+//            isUpdate: false,
+//            isNew: true
+//         )
+//      ]
+//   }
+//}
