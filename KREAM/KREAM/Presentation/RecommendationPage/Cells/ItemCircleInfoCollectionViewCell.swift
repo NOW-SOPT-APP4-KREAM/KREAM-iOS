12de3dc5392e73a8279ecb033816f9a403c8075a
FpRaArNkK <mspark1184@naver.com>
2024-05-24 21:27:33 +0900

    Feat: #54 ItemCircleInfoCollectioonViewCell init

---
+++ KREAM/KREAM/Presentation/RecommendationPage/Cells/ItemCircleInfoCollectionViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  ItemCircleInfoCollectionViewCell.swift
+//  KREAM
+//
+//  Created by 박민서 on 5/24/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class ItemCircleInfoCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Properties
+    // MARK: Views
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+        setUpLayout()
+        setUpStyle()
+        setUpConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: setUpViews
+    private func setUpViews() {
+        
+    }
+    // MARK: setUpLayout
+    private func setUpLayout() {
+        
+    }
+    // MARK: setUpStyle
+    private func setUpStyle() {
+        
+    }
+    // MARK: setUpConstraint
+    private func setUpConstraint() {
+        
+    }
+}
+
