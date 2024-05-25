d6586b4c5cc884d64620e21f9c98dd05fa8e215e
0gonge <Challengef0802@gmail.com>
2024-05-25 23:33:07 +0900

    Feat:64 - MoreStyle버튼 추가

---
+++ KREAM/KREAM/Presentation/DetailInfoPage/Views/MoreStyleView.swift
@@ -0,0 +1,39 @@
+//
+//  MoreStyleView.swift
+//  KREAM
+//
+//  Created by 송여경 on 5/25/24.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class MoreStyleView : UIView {
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Set Elements
+    private let moreStyle = UIButton()
+    
+    //MARK: - SetUp
+    private func setupStyle(){
+        moreStyle.do {
+            $0.setTitle("스타일 더보기", for: .normal)
+            $0.setTitleColor(.black02, for: .normal)
+            $0.titleLabel?.font = .kreamFont(.body4(.semibold, isLh150: true))
+            $0.backgroundColor = .white
+            $0.layer.borderColor = UIColor.gray04.cgColor
+            $0.layer.borderWidth = 1
+            $0.layer.cornerRadius = 10
+        }
+    }
+
+}
