b76d19f643ddecb1d633fd8505c0ddb3ad6008e5
ParkSY0919 <qhr498@gmail.com>
2024-05-31 16:31:45 +0900

    [Fix]: rootViewController 수정

--- KREAM/KREAM/Application/SceneDelegate.swift
+++ KREAM/KREAM/Application/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
-        self.window?.rootViewController = ForthViewController()
+        self.window?.rootViewController = AppTabBarController()
         self.window?.makeKeyAndVisible()
     }
 
